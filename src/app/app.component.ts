96cec346d28f4773ec0e5606c17157046a2afc52
Panchal, Pushpakkumar <pushpak.net@gmail.com>
2020-04-07 19:43:28 -0500

    login page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { NgForm, EmailValidator } from '@angular/forms';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +11,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'login-page';
+
+  constructor(private http: HttpClient, private router: Router ) {
+
+  }
+  incorrectCreds: boolean = false;
+  email: any;
+
+  onLogin(form: NgForm) {
+    this.email = form.value.Email;
+    const sn = form.value.SerialNumber;
+//?email=lacey@torx.com&sn=100017730
+    this.http.get("http://buds2software.com/api/values", { responseType: 'text', observe: 'response', params: { email: this.email, sn: sn } }).subscribe((data) => {
+      console.log(data);  
+      console.log(data.status);
+ 
+        this.incorrectCreds = false;
+        console.log(data); 
+        sessionStorage.setItem('isUserAuthenticated', 'true');
+        sessionStorage.setItem('userName', sn);
+        
+        window.location.href='http://downloads.buds2software.com';
+ 
+
+    }, (error) => {
+
+      this.incorrectCreds = true;
+    })
+
+
+  }
 }
